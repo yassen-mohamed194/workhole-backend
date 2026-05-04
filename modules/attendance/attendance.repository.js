7b9fce5cdacfda1b38ba9de8b9fe1e0601ab8fde
yassen-mohamed194 <yasenze89@gmail.com>
2026-05-04 04:30:03 +0300

    Remove attendance repository functions and related model import, streamlining the attendance module.

--- modules/attendance/attendance.repository.js
+++ modules/attendance/attendance.repository.js
@@ -1,14 +1,1 @@
-const Attendance = require('./attendance.model');
 
-function createAttendance(payload) {
-  return Attendance.create(payload);
-}
-
-function findAllAttendance() {
-  return Attendance.find().sort({ createdAt: -1 });
-}
-
-module.exports = {
-  createAttendance,
-  findAllAttendance,
-};
